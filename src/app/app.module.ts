b5b58fa8b60c95fcad9a60e3ae69352c9e6ca5be
xratq <baranovasia@gmail.com>
2022-05-16 13:42:51 +0300

    da

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {Routes, RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { ForkComponent } from './fork/fork.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+const appRoutes: Routes =[
+    { path: '', component: AppComponent},
+    { path: 'korp1', component: ForkComponent },
+    { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ForkComponent
+    ForkComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,7 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'not-found-app',
+    template: `<h3>Страница не найдена</h3>`
+})
+export class NotFoundComponent { }
