c43b5eeb2eedbfc3e2e28f81ebec0e9906114c5c
xratq <baranovasia@gmail.com>
2022-05-17 11:43:21 +0300

    it works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,9 @@ import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { ForkComponent } from './fork/fork.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 const appRoutes: Routes =[
-    { path: '', component: AppComponent},
+    { path: '', component: HomeComponent},
     { path: 'korp1', component: ForkComponent },
     { path: '**', component: NotFoundComponent }
 ];
@@ -16,7 +17,8 @@ const appRoutes: Routes =[
   declarations: [
     AppComponent,
     ForkComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {}
